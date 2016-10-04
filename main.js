273ded142e2e0fe0c987f8d2105258ac1ad9ae6c
Grace Lee <eunheh@gmail.com>
2016-10-04 16:15:57 -0400

    calculator is working

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 
 
-function add (x,y) {
-  return (x = y);
-}
+var button = document.getElementById("calculate-button")
+var answer = document.getElementById("answer")
+
+
+var calculate = function () {
+  var firstNum = document.getElementById("value-one");
+  var secondNum = document.getElementById("value-two");
+  var x = Number(firstNum.value);
+  var y = Number(secondNum.value);
+  var sum = (x + y);
+  answer.innerHTML = sum;
+};
+
+button.addEventListener("click", calculate);
+
+//long method if I used runCalc(x + y); on line 12
+// function runCalc (x, y) {
+//   var sum = x + y;
+//   answer.innerHTML = sum;
+// }
