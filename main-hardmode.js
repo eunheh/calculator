5366db6723cd48583b3382da24728c8bbc89f561
Grace Lee <eunheh@gmail.com>
2016-10-04 23:58:34 -0400

    attempt at hard mode, didn't get to much js

---
+++ main-hardmode.js
@@ -0,0 +1,20 @@
+
+
+var calculator = document.getElementByClass("calculate-button")
+var 0 = document.getElementByClass("0")
+var 1 = document.getElementByClass("1")
+var 2 = document.getElementByClass("2")
+var 3 = document.getElementByClass("3")
+var 4 = document.getElementByClass("4")
+var 5 = document.getElementByClass("5")
+var 6 = document.getElementByClass("6")
+var 7 = document.getElementByClass("7")
+var 8 = document.getElementByClass("8")
+var 9 = document.getElementByClass("9")
+
+var calculate = function () {
+  if
+
+};
+
+button.addEventListener("click", calculate);
